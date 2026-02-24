939c9b9d73ed736367898218cd59e8a77f52a727
Ena Grubor <enagrubor01@gmail.com>
2026-02-24 12:07:33 +0100

    Euclidean.py

--- Euclidean.py
+++ Euclidean.py
@@ -8,13 +8,12 @@ from metric_learn import LFDA
 import warnings
 warnings.filterwarnings("ignore", category=FutureWarning)
 
-# 1. Putanje do instrumenata
+# 1. Paths to the instruments
+data_path = "C:/Users/User/Desktop/New Folder/DATA/Train_submission"
+instruments = ["drum","guitar", "piano", "violin"]  # 4 instruments
 
-data_path = "C:/Users/User/Desktop/diplomski rad/DATA2/Train_submission"
-instruments = ["drum","guitar", "piano", "violin"]  # 4 instrumenta
 
-
-# 2. Funkcija za izračunavanje MFCC + ZCR + Spectral Centroid + Spectral Bandwidth + Spectral Roll-off
+# 2. Function for MFCC + ZCR + Spectral Centroid + Spectral Bandwidth + Spectral Roll-off
 no_k = 16
 def extract_features(file_path, n_mfcc=no_k):
     y, sr = librosa.load(file_path, duration=3)
@@ -44,7 +43,7 @@ def extract_features(file_path, n_mfcc=no_k):
     sr_mean = np.mean(sr_roll)
     sr_std = np.std(sr_roll)
 
-    # === Kombinuj sve u jedan vektor ===
+    # === All in one vector ===
     feature_vector = np.hstack([
         mfcc_mean, mfcc_std,           # 13 + 13 = 26
         [zcr_mean, zcr_std],           # 2
@@ -57,10 +56,10 @@ def extract_features(file_path, n_mfcc=no_k):
 
 
 
-# 3. Učitavanje svih fajlova
+# 3. Loading all files
 
-X = []  # feature matrica
-y = []  # labele (naziv instrumenta)
+X = []  # feature matrix
+y = []  # labels (names of the instruments)
 
 for label, instr in enumerate(instruments):
     folder = os.path.join(data_path, instr)
@@ -82,13 +81,13 @@ print("Shape y:", y.shape)
 
 from sklearn.preprocessing import StandardScaler
 
-# === NORMALIZACIJA (standardizacija) ===
+# === NORMALIZATION ===
 scaler = StandardScaler()
 X = scaler.fit_transform(X)
 
 
 
-# Mapa engleski → srpski
+# Map for english → serbian
 instrument_map = {
     "drum": "bubnjevi",
     "guitar": "gitara",
@@ -96,22 +95,22 @@ instrument_map = {
     "violin": "violina"
 }
 
-# Kreiraj listu srpskih naziva prema redosledu u instruments
+# Create a list of Serbian names according to order in instruments
 instruments_srpski = [instrument_map[i] for i in instruments]
 
 
 '''
 
-# === STATISTIKA PRE BALANSIRANJA===
+# === STATISTICS BEFORE BALANCING ===
 counts = [np.sum(y == i) for i in range(len(instruments))]
 
-# Bar chart sa brojevima na stubovima
+# Bar chart
 plt.figure(figsize=(8, 5))
 bars = plt.bar(instruments_srpski, counts, color=['red','blue','green','orange'])
-plt.title("Broj primera po instrumentu")
-plt.ylabel("Broj fajlova")
+plt.title("Number of examples per instrument")
+plt.ylabel("Number of files")
 
-# Dodaj brojeve iznad stubova
+# with numbers
 for bar, count in zip(bars, counts):
     plt.text(bar.get_x() + bar.get_width()/2, bar.get_height() + 5,
              str(count), ha='center', va='bottom', fontsize=10, fontweight='bold')
@@ -123,32 +122,32 @@ plt.show()
 
 from imblearn.over_sampling import SMOTE
 
-# === Balansiranje klasa SMOTE-om ===
-smote = SMOTE(sampling_strategy='all', random_state=42)  # random_state za reproduktivnost
+# === Balancing classes with SOMTE ===
+smote = SMOTE(sampling_strategy='all', random_state=42)
 #smote = SMOTE(sampling_strategy='all')
 X, y = smote.fit_resample(X, y)
 
-print("Nakon SMOTE-a, shape X:", X.shape)
+print("After SMOTE, shape X:", X.shape)
 
-# Broj primera po klasi sa imenima instrumenata
+# Number of examples per class with instrument names
 counts = [np.sum(y == i) for i in range(len(instruments))]
 counts_dict = {instr: count for instr, count in zip(instruments_srpski, counts)}
 
-print("Broj primera po klasi nakon SMOTE-a:",
+print("Number of examples per class after SMOTE:",
       [f"{instr}:{count}" for instr, count in counts_dict.items()])
 
 '''
-# === STATISTIKA ===
+# === STATISTICS ===
 counts = [np.sum(y == i) for i in range(len(instruments))]
 
-# Bar chart sa brojevima na stubovima
+# Bar chart
 plt.figure(figsize=(8, 5))
 #bars = plt.bar(instruments, counts, color=['red','blue','green','orange'])
 bars = plt.bar(instruments_srpski, counts, color=['red', 'blue', 'green', 'orange'])
-plt.title("Broj primera po instrumentu")
-plt.ylabel("Broj fajlova")
+plt.title("Number of examples per instrument")
+plt.ylabel("Number of files")
 
-# Dodaj brojeve iznad stubova
+# with numbers
 for bar, count in zip(bars, counts):
     plt.text(bar.get_x() + bar.get_width()/2, bar.get_height() + 5,
              str(count), ha='center', va='bottom', fontsize=10, fontweight='bold')
@@ -165,24 +164,24 @@ plt.show()
 #------------------------------------------------------------------------------------------------
 
 
-# 4. LDA redukcija 2D
+# 4. LDA reduction 2D
 lda = LinearDiscriminantAnalysis(n_components=2)
 X_lda = lda.fit_transform(X, y)
 
 
-# 5. LFDA redukcija 2D
+# 5. LFDA redukction 2D
 lfda = LFDA(n_components=2, k=5)
 X_lfda = lfda.fit_transform(X, y)
 
 
-# 6. Crtanje poređenja
+# 6. Comparison
 fig, axs = plt.subplots(1, 2, figsize=(16, 6))
 colors = ['red', 'blue', 'green', 'orange']
 
 # LDA 2D
 for i, instr in enumerate(instruments_srpski):
     axs[0].scatter(X_lda[y==i, 0], X_lda[y==i, 1], label=instr, alpha=0.7, color=colors[i])
-axs[0].set_title("LDA redukcija MFCC 2D")
+axs[0].set_title("LDA reduction MFCC 2D")
 axs[0].set_xlabel("LDA1")
 axs[0].set_ylabel("LDA2")
 axs[0].legend()
@@ -190,7 +189,7 @@ axs[0].legend()
 # LFDA 2D
 for i, instr in enumerate(instruments_srpski):
     axs[1].scatter(X_lfda[y==i, 0], X_lfda[y==i, 1], label=instr, alpha=0.7, color=colors[i])
-axs[1].set_title("LFDA redukcija MFCC 2D")
+axs[1].set_title("LFDA reduction MFCC 2D")
 axs[1].set_xlabel("LFDA1")
 axs[1].set_ylabel("LFDA2")
 axs[1].legend()
@@ -200,16 +199,16 @@ plt.show()
 
 
 
-# LDA redukcija 3D
+# LDA reduction 3D
 lda3 = LinearDiscriminantAnalysis(n_components=3)
 X_lda3 = lda3.fit_transform(X, y)
 
-# LFDA redukcija 3D
+# LFDA reduction 3D
 lfda3 = LFDA(n_components=3, k=5)
 X_lfda3 = lfda3.fit_transform(X, y)
 
 
-# Crtanje 3D poređenja
+# Comparison
 fig = plt.figure(figsize=(16, 7))
 
 # LDA 3D subplot
@@ -224,7 +223,7 @@ for i, instr in enumerate(instruments_srpski):
         s=50,
         color=colors[i]
     )
-ax1.set_title("LDA redukcija MFCC 3D")
+ax1.set_title("LDA reduction MFCC 3D")
 ax1.set_xlabel("LDA1")
 ax1.set_ylabel("LDA2")
 ax1.set_zlabel("LDA3")
@@ -242,7 +241,7 @@ for i, instr in enumerate(instruments_srpski):
         s=50,
         color=colors[i]
     )
-ax2.set_title("LFDA redukcija MFCC 3D")
+ax2.set_title("LFDA reduction MFCC 3D")
 ax2.set_xlabel("LFDA1")
 ax2.set_ylabel("LFDA2")
 ax2.set_zlabel("LFDA3")
@@ -259,26 +258,27 @@ plt.show()
 
 
 
-# --- FILTRACIJA TEST FAJLOVA (više labela podržano) ---
+# --- FILTRATION OF TEST FILES (multiple labels supported) ---
 import pandas as pd
 import os
 import numpy as np
 
-# --- Učitavanje putanja ---
-test_path = "C:/Users/User/Desktop/diplomski rad/DATA2/Test_submission/Test_submission"
-metadata_path = "C:/Users/User/Desktop/diplomski rad/DATA2/Metadata_Test.csv"
+# --- Loading paths ---
+test_path = "C:/Users/User/Desktop/New Folder/DATA/Test_submission/Test_submission"
+metadata_path = "C:/Users/User/Desktop/New Folder/DATA/Metadata_Test.csv"
 
-# --- Učitavanje metapodataka iz CSV fajla ---
+# --- Loading metadata from a CSV file ---
 metadata = pd.read_csv(metadata_path)
-print("Učitano redova iz metadata:", len(metadata))
+print("Loaded rows from metadata:", len(metadata))
 
-# --- Kreiranje test skupa ---
+# --- Creating test set ---
 X_test = []
 y_test = []
 
-# Lista instrumenata (isti redosled kao u treniranju)
+
+# List of instruments (same order as in training)
 #instruments = ["piano", "violin", "drum", "guitar"]
-instruments = ["drum","guitar", "piano", "violin"]  # 4 instrumenta
+instruments = ["drum","guitar", "piano", "violin"]  # 4 instruments
 
 
 for _, row in metadata.iterrows():
@@ -287,23 +287,23 @@ for _, row in metadata.iterrows():
 
     wav_path = os.path.join(test_path, file_name)
     if not os.path.exists(wav_path):
-        print(f"Upozorenje: fajl {file_name} nije pronađen, preskačem.")
+        print(f"Warning: file {file_name} not found, skip.")
         continue
 
-    # Ekstrakcija karakteristika (MFCC)
+    # Feature extraction (MFCC)
     features = extract_features(wav_path)
 
     X_test.append(features)
     y_test.append(instruments.index(label))
 
-# Pretvaranje u numpy nizove
+# Converting to numpy arrays
 X_test = np.array(X_test)
 y_test = np.array(y_test)
 
 print("Test shape:", X_test.shape)
 print("Broj različitih labela:", len(np.unique(y_test)))
 
-# === Normalizacija test skupa istim scalerom ===
+# === Normalization of the test set with the same scaler ===
 X_test = scaler.transform(X_test)
 
 
@@ -316,16 +316,16 @@ from sklearn.metrics import accuracy_score, confusion_matrix, ConfusionMatrixDis
 
 # KNN
 
-# === KNN na originalnim MFCC ===
+# === KNN original MFCC ===
 
-#knn_raw = KNeighborsClassifier(n_neighbors=3) # euklidska
-knn_raw = KNeighborsClassifier(n_neighbors=3, metric='euclidean') # euklidska
-#knn_raw = KNeighborsClassifier(n_neighbors=3, weights='distance') # euklidska oteženjena
+#knn_raw = KNeighborsClassifier(n_neighbors=3) # euclidean
+knn_raw = KNeighborsClassifier(n_neighbors=3, metric='euclidean') # euclidean
+#knn_raw = KNeighborsClassifier(n_neighbors=3, weights='distance') # euclidean weighted
 
 knn_raw.fit(X, y)
 y_pred_raw = knn_raw.predict(X_test)
 acc_raw = accuracy_score(y_test, y_pred_raw)
-print("KNN tačnost (MFCC):", round(acc_raw, 3))
+print("KNN accuracy (MFCC):", round(acc_raw, 3))
 cm_raw = confusion_matrix(y_test, y_pred_raw)
 disp = ConfusionMatrixDisplay(cm_raw, display_labels=instruments_srpski)
 disp.plot(cmap="Blues")
@@ -333,23 +333,23 @@ disp.plot(cmap="Blues")
 disp.ax_.tick_params(axis='x', labelsize=12)
 disp.ax_.tick_params(axis='y', labelsize=12)
 
-# Povećaj brojeve unutar same matrice
+
 for text in disp.text_.ravel():
-    text.set_fontsize(13)          # promena veličine fonta
-    text.set_fontweight('bold')    # po želji – podebljaj tekst
+    text.set_fontsize(13)          
+    text.set_fontweight('bold')
 
-# Nazivi osa
-disp.ax_.set_xlabel("Predviđena klasa", fontsize=13)
-disp.ax_.set_ylabel("Stvarna klasa", fontsize=13)
 
-plt.title("KNN - Originalni MFCC (Euklidska distanca)", fontsize=14, fontweight = 'bold')
-#plt.title("KNN - Originalni MFCC (Euklidska distanca, oteženjena)", fontsize=13, fontweight = 'bold')
+disp.ax_.set_xlabel("Predicted class", fontsize=13)
+disp.ax_.set_ylabel("Real class", fontsize=13)
+
+plt.title("KNN - Original MFCC (Euclidean distance)", fontsize=14, fontweight = 'bold')
+#plt.title("KNN - Original MFCC (Euclidean distance, weighted)", fontsize=13, fontweight = 'bold')
 plt.show()
 
 
 
-# === KNN na LDA redukovanim podacima ===
-# Moramo transformisati i test skup pomoću istog lda modela
+# === KNN on LDA reduced data ===
+# We must also transform the test set using the same lda model
 X_test_lda = lda.transform(X_test)
 knn_lda = KNeighborsClassifier(n_neighbors=3)
 #knn_lda = KNeighborsClassifier(n_neighbors=3, weights='distance')
@@ -357,7 +357,7 @@ knn_lda = KNeighborsClassifier(n_neighbors=3)
 knn_lda.fit(X_lda, y)
 y_pred_lda = knn_lda.predict(X_test_lda)
 acc_lda = accuracy_score(y_test, y_pred_lda)
-print("KNN tačnost (LDA):", round(acc_lda, 3))
+print("KNN accuracy (LDA):", round(acc_lda, 3))
 cm_lda = confusion_matrix(y_test, y_pred_lda)
 disp = ConfusionMatrixDisplay(cm_lda, display_labels=instruments_srpski)
 disp.plot(cmap="Oranges")
@@ -365,23 +365,23 @@ disp.plot(cmap="Oranges")
 disp.ax_.tick_params(axis='x', labelsize=12)
 disp.ax_.tick_params(axis='y', labelsize=12)
 
-# Povećaj brojeve unutar same matrice
+
 for text in disp.text_.ravel():
-    text.set_fontsize(13)          # promena veličine fonta
-    text.set_fontweight('bold')    # po želji – podebljaj tekst
+    text.set_fontsize(13)
+    text.set_fontweight('bold') 
 
-# Nazivi osa
-disp.ax_.set_xlabel("Predviđena klasa", fontsize=13)
-disp.ax_.set_ylabel("Stvarna klasa", fontsize=13)
 
-plt.title("KNN - LDA redukcija (Euklidska distanca)", fontsize=14, fontweight = 'bold')
-#plt.title("KNN - LDA redukcija (Euklidska distanca, oteženjena)", fontsize=13, fontweight = 'bold')
+disp.ax_.set_xlabel("Predicted class", fontsize=13)
+disp.ax_.set_ylabel("Real class", fontsize=13)
+
+plt.title("KNN - LDA reduction (Euclidean distance)", fontsize=14, fontweight = 'bold')
+#plt.title("KNN - LDA reduction (Euclidean distance, weighted)", fontsize=13, fontweight = 'bold')
 plt.show()
 
 
 
-# === KNN na LFDA redukovanim podacima ===
-# Moramo transformisati i test skup pomoću istog lfda modela
+# === KNN on LFDA reduced data ===
+# We must also transform the test set using the same lfda model
 X_test_lfda = lfda.transform(X_test)
 knn_lfda = KNeighborsClassifier(n_neighbors=3)
 #knn_lfda = KNeighborsClassifier(n_neighbors=3, weights='distance')
@@ -389,7 +389,7 @@ knn_lfda = KNeighborsClassifier(n_neighbors=3)
 knn_lfda.fit(X_lfda, y)
 y_pred_lfda = knn_lfda.predict(X_test_lfda)
 acc_lfda = accuracy_score(y_test, y_pred_lfda)
-print("KNN tačnost (LFDA):", round(acc_lfda, 3))
+print("KNN accuracy (LFDA):", round(acc_lfda, 3))
 cm_lfda = confusion_matrix(y_test, y_pred_lfda)
 disp = ConfusionMatrixDisplay(cm_lfda, display_labels=instruments_srpski)
 disp.plot(cmap="Greens")
@@ -397,37 +397,37 @@ disp.plot(cmap="Greens")
 disp.ax_.tick_params(axis='x', labelsize=12)
 disp.ax_.tick_params(axis='y', labelsize=12)
 
-# Povećaj brojeve unutar same matrice
+
 for text in disp.text_.ravel():
-    text.set_fontsize(13)          # promena veličine fonta
-    text.set_fontweight('bold')    # po želji – podebljaj tekst
+    text.set_fontsize(13)
+    text.set_fontweight('bold')
 
-# Nazivi osa
-disp.ax_.set_xlabel("Predviđena klasa", fontsize=13)
-disp.ax_.set_ylabel("Stvarna klasa", fontsize=13)
 
-plt.title("KNN - LFDA redukcija (Euklidska distanca)", fontsize=14, fontweight = 'bold')
-#plt.title("KNN - LFDA redukcija (Euklidska distanca, oteženjena)", fontsize=13, fontweight = 'bold')
+disp.ax_.set_xlabel("Predicted class", fontsize=13)
+disp.ax_.set_ylabel("Real class", fontsize=13)
+
+plt.title("KNN - LFDA reduction (Euclidean distance)", fontsize=14, fontweight = 'bold')
+#plt.title("KNN - LFDA reduction (Euclidean distance, weighted)", fontsize=13, fontweight = 'bold')
 plt.show()
 
 
 
-# === Poređenje tačnosti ===
-print("\n--- Rezime tačnosti (k=3) ---")
-print(f"Originalni MFCC: {acc_raw:.3f}")
-print(f"LDA redukcija:   {acc_lda:.3f}")
-print(f"LFDA redukcija:  {acc_lfda:.3f}")
+# === Comparison of accuracy ===
+print("\n--- Accuracy summary (k=3) ---")
+print(f"Original MFCC: {acc_raw:.3f}")
+print(f"LDA redukction:   {acc_lda:.3f}")
+print(f"LFDA reduction:  {acc_lfda:.3f}")
 
 #---------------------------------------------------------------------------------------------------------------
 
-# === TESTIRANJE RAZLIČITIH VREDNOSTI k ===
+# === TESTING DIFFERENT VALUES OF k ===
 k_values = [1, 3, 5, 7, 9, 11]
 results = []
 
-print("\n--- Eksperiment: promena broja komšija (k) ---")
+print("\n--- Experiment: changing the number of neighbors (k) ---")
 
 for k in k_values:
-    # === Originalni MFCC ===
+    # === Original MFCC ===
     #knn_raw = KNeighborsClassifier(n_neighbors=k)
     knn_raw = KNeighborsClassifier(n_neighbors=k, weights='distance')
     knn_raw.fit(X, y)
@@ -450,15 +450,16 @@ for k in k_values:
     results.append((k, acc_raw, acc_lda, acc_lfda))
     print(f"k={k:2d} → MFCC={acc_raw:.3f}, LDA={acc_lda:.3f}, LFDA={acc_lfda:.3f}")
 
-# === Crtanje rezultata ===
+# === Plotting the results ===
 results = np.array(results)
 plt.figure(figsize=(8,5))
 plt.plot(results[:,0], results[:,1], '-o', label="MFCC", color='blue')
 plt.plot(results[:,0], results[:,2], '-o', label="LDA", color='orange')
 plt.plot(results[:,0], results[:,3], '-o', label="LFDA", color='green')
-plt.xlabel("Broj komšija (k)", fontsize=13)
-plt.ylabel("Tačnost", fontsize=13)
-plt.title("Uticaj broja komšija na tačnost KNN klasifikatora", fontsize=14, fontweight = 'bold')
+plt.xlabel("Number of neighbors (k)", fontsize=13)
+plt.ylabel("Accuracy", fontsize=13)
+plt.title("The influence of the number of neighbors on the accuracy of the KNN classifier", fontsize=14, fontweight = 'bold')
 plt.legend()
 plt.grid(True)
 plt.show()
+
